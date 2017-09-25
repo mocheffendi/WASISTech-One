e4244b8f219b5698a9556b3a19bdbf86f5449ef4
mocheffendi <moch.effendi@gmail.com>
2017-09-25 19:48:36 +0700

    Update FirstMedia Samsung HomeCable UNO MEGA.ino

--- FirstMedia Samsung HomeCable UNO MEGA.ino
+++ FirstMedia Samsung HomeCable UNO MEGA.ino
@@ -1,43 +1,43 @@
 //NECX
-ON 4F587887
-MailBox 4F58748B
-Resolution 4F58F40B
-0 4F58906F
-1 4F5800FF
-2 4F58807F
-3 4F5840BF
-4 4F58C03F
-5 4F5820DF
-6 4F58A05F
-7 4F58609F
-8 4F58E01F
-9 4F5810EF
-Return 4F580CF3
-Epg 4F58D827
-Lang 4F588C73
-Disp 4F584CB3
-Opt 4F58CC33
-App 4F58AC53
-Menu 4F586C93
-Media 4F58D42B
-Vod 4F58EC13
-VolUp 4F5850AF
-VolDown 4F58D02F
-Mute 4F5804FB
-CHUp 4F5830CF
-CHDown 4F58B04F
-PreCH 4F5834CB
-Info 4F58A45B
-Up 4F5808F7
-Right 4F58C837
-Left 4F5848B7
-Down 4F588877
-OK 4F5828D7
-Back 4F581CE3
-PlayPause 4F58F00F
-TVRadio 4F58847B
-Exit 4F58A857
-Red 4F58649B
-Green 4F5844BB
-Yellow 4F58C43B
-Blue 4F5824DB
+#define ON 4F587887
+#define MailBox 4F58748B
+#define Resolution 4F58F40B
+#define angka0 4F58906F
+#define angka1 4F5800FF
+#define angka2 4F58807F
+#define angka3 4F5840BF
+#define angka4 4F58C03F
+#define angka5 4F5820DF
+#define angka6 4F58A05F
+#define angka7 4F58609F
+#define angka8 4F58E01F
+#define angka9 4F5810EF
+#define Return 4F580CF3
+#define Epg 4F58D827
+#define Lang 4F588C73
+#define Disp 4F584CB3
+#define Opt 4F58CC33
+#define App 4F58AC53
+#define Menu 4F586C93
+#define Media 4F58D42B
+#define Vod 4F58EC13
+#define VolUp 4F5850AF
+#define VolDown 4F58D02F
+#define Mute 4F5804FB
+#define CHUp 4F5830CF
+#define CHDown 4F58B04F
+#define PreCH 4F5834CB
+#define Info 4F58A45B
+#define Up 4F5808F7
+#define Right 4F58C837
+#define Left 4F5848B7
+#define Down 4F588877
+#define OK 4F5828D7
+#define Back 4F581CE3
+#define PlayPause 4F58F00F
+#define TVRadio 4F58847B
+#define Exit 4F58A857
+#define Red 4F58649B
+#define Green 4F5844BB
+#define Yellow 4F58C43B
+#define Blue 4F5824DB
