183ab244cbfe557f2a992fe793cabdb9c14eeedb
mocheffendi <moch.effendi@gmail.com>
2017-09-28 06:44:23 +0700

    Create LG TV InfraRed Code.ino

---
+++ LG TV InfraRed Code.ino
@@ -0,0 +1,52 @@
+//LG TV InfraRed Code
+
+#define OnOff   0x20DF10EF
+#define CHUp    0x20DF00FF
+#define CHDown  0x20DF807F
+#define VolUp   0x20DF40BF
+#define VolDown 0x20DFC03F
+#define Mute    0x20DF906F
+#define Ok      0x20DF22DD
+
+#define 1       0x20DF8877
+#define 2       0x20DF48B7
+#define 3       0x20DFC837
+#define 4       0x20DF28D7
+#define 5       0x20DFA857
+#define 6       0x20DF6897
+#define 7       0x20DFE817
+#define 8       0x20DF18E7
+#define 9       0x20DF9867
+#define 0       0x20DF08F7
+
+#define Setting 0x20DFC23D
+#define Up      0x20DF02FD
+#define Down    0x20DF827D
+#define Left    0x20DFE01F
+#define Right   0x20DF609F
+
+#define Subtitle0x20DF9C63 
+#define AVmode  0x20DF0CF3
+#define Ratio   0x20DF9E61
+#define Input   0x20DFD02F
+#define List    0x20DFCA35
+#define QView   0x20DF58A7
+#define FAV     0x20DF7887
+#define Guide   0x20DFD52A
+#define Text    0x20DF04FB
+#define Info    0x20DF55AA
+#define TOpt    0x20DF847B
+#define QMenu   0x20DFA25D
+#define Back    0x20DF14EB
+#define Exit    0x20DFDA25
+#define Simplink0x20DF7E81
+#define Sleep   0x20DF708F
+#define Stop    0x20DF8D72
+#define Backward0x20DFF10E
+#define Play    0x20DF0DF2
+#define Pause   0x20DF5DA2
+#define Forward 0x20DF718E
+#define Red     0x20DF4EB1
+#define Green   0x20DF8E71
+#define Yellow  0x20DFC639
+#define Blue    0x20DF8679
